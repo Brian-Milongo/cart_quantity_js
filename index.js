affa508ff22d68ce0276e3fd1991200acb5a1824
Brian Otieno <brianotieno1021@gmail.com>
2023-06-12 17:35:44 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let person = {
+    name: 'Brian',
+    age: 26
+
+};
+
+
+person.name = 'Emilly';
+console.log(person.name);
+
+
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+greet('Brian', 'Otieno');
+
+//alert('Hello');
+Math.round(2.8)
+console.log(2 + 2);
+console.log('some' + 'text');
